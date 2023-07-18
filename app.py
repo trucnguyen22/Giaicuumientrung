50cd2ac6b739db75da6cecd63a3c31f35533432e
trucnguyen22 <trucnguyenytc@gmail.com>
2023-07-18 10:11:25 +0700

    18/7

--- app.py
+++ app.py
@@ -1,55 +1,45 @@
 from flask import Flask, render_template, request, jsonify
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-'''app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:minhtruc@localhost/sampledb'
-db = SQLAlchemy(app)'''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:minhtruc@localhost/'
+db = SQLAlchemy(app)
 
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from the url parameter /getmsg/?name=
-    name = request.args.get("name", None)
+class Student(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(40))
+    lname = db.Column(db.String(40))
+    email = db.Column(db.String(40))
 
-    # For debugging
-    print(f"Received: {name}")
-
-    response = {}
-
-    # Check if the user sent a name at all
-    if not name:
-        response["ERROR"] = "No name found. Please send a name."
-    # Check if the user entered a number
-    elif str(name).isdigit():
-        response["ERROR"] = "The name can't be numeric. Please send a string."
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {param} to our awesome API!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "No name found. Please send a name."
-        })
+    def __init__(self, fname, lname, email):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
 
 
 @app.route('/')
 def index():
-    # A welcome message to test our server
-    return render_template("login.html")
+    return render_template('login.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    fname = request.form['fname']
+    lname = request.form['lname']
+    email = request.form['email']
+
+    student = Student(fname, lname, email)
+    db.session.add(student)
+    db.session.commit()
+
+    # fetch a certain student
+    studentResult = db.session.query(Student).filter(Student.id == 1)
+    for result in studentResult:
+        print(result.fname)
+
+    return render_template('verify.html', data=fname)
 
 
 if __name__ == '__main__':
